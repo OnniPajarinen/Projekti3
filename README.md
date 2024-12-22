e0ff0abc00b3d5e98c05dd1188faee6514ccceeb
OnniPajarinen <onni.pajarinen@student.laurea.fi>
2024-12-22 21:09:25 +0200

    l

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 <h1>Projekti 3</h1>
 
 # Project Title 
-The name of the Project and all authors involved
+Exchange The Currency Converter
 
 ## Demo link:
 Access my site at [google.com](https://google.com)
 
 ## How was the workload divided
-How was the workload divided. Who did what? 
-
+My work was all done by me.
 
 
 ## Table of Content:
@@ -23,34 +22,27 @@ How was the workload divided. Who did what?
 - [License](#license)
 
 ## About The App
-[Name of project] is an app that ...
+Exchange The Currency Converter is an app that converts USD to other currencies and you can even choose the amount you want to convert.
 
 ## Screenshots
-Add here atleast one screenshot of the working application 
-`![Writing](https://unsplash.com/photos/VBPzRgd7gfc)`
+![picture of the app](kouluu.png)
 
-Picture by [Kelly Sikkema](https://unsplash.com/@kellysikkema)
+Picture by [Onni Pajarinen]  
 
 ## Technologies
 Describe which technologies were used and the role of each in your project. 
 I used the following technologies `html`, `css`, ...
 
 ## Setup
-Write brief instructions on how to run and use your app. For example:
-- download all the files and open index.html in your browser
-- download or clone the repository
-- run `npm install`
-- ...
+My application is Exchange The Currency Converter and it can convert USD currency to currencies of other countries. It works by entering any number greater than 0 into the box. Then you select a currency from the dropdown menu to which you want to convert dollars. The application also shows some extra exchange rates, but you can hide or unhide them from the button that says "Hide/Show the extra rates!".
 
 ## Status
-[name of project] is still in progress. `Version 2` will be out soon.
+Exchange The Currency Converter is still in progress. `Version 2` will be out soon where there you can convert other currencies to other currencies instead of USD.
 
 ## Credits
-List of contriubutors and sources you used during the project. Also mention if you used ChatGPT or other AI tools when coding and explain how did you benefit from them.
-- [John Doe](johndoe.com)
+I asked what should I do from ChatGPT since I didn't know what app should I make. Everything was done by me at the end, ChatGPT helped in some situation where I didn't know what was wrong with my code because it didn't show up right.
+- [Onni Pajarinen]
 - [Beginners guide to BEM](link-goes-here.com)
 
 ## License
-Choose a license for your project by following this [guide](https://docs.github.com/en/communities/setting-up-your-project-for-healthy-contributions/adding-a-license-to-a-repository).
-
-Example: MIT license @ [author](author.com)
+MIT license @ [Onni Pajarinen]
--- Script/index.js
+++ Script/index.js
@@ -1,3 +1,4 @@
+// Kyseinen funktio tekee sen, että se tuo APIsta vedetyn xml tiedoston tiedot kaikista valuutoista ja paljonko saa yhdellä USD.
 function fetchRates() {
     $.ajax({
         url: "https://openexchangerates.org/api/latest.json?app_id=1c24f6bc0a1a4d8da2da59ca56c752d8",
@@ -5,6 +6,8 @@ function fetchRates() {
         success: function (response) {
             var rates = response.rates;
 
+            // Lisäilin eniten käytettyjä valuuttoja jo listan ekoille paikoille ja laitoin myös Select napin ja - napin,
+            // mistä ei voi klikata, mutta se näyttää etsiessä valuuttoja siistimmältä.
             $("#menu").html('<option value="" selected disabled>Select</option>');
             $("#menu").append('<option value="EUR">EUR</option>')
             $("#menu").append('<option value="JPY">JPY</option>')
@@ -14,6 +17,7 @@ function fetchRates() {
             $("#menu").append('<option value="INR">INR</option>')
             $("#menu").append('<option value="" disabled>-</option>')
 
+            // Tämä on jQuery UIta ja se tekee määrän syöttölaatikosta hienomman.
             $("#multiply").spinner({
                 min: 1,
                 max: 1000,
@@ -23,16 +27,19 @@ function fetchRates() {
                 }
             });
 
-            $.each(rates, function (currency, rate) {
+            //Tämä kalastaa juuri ne valuutat ja niitten raten xml tiedostosta.
+            $.each(rates, function (currency) {
                 $("#menu").append(`<option value="${currency}">${currency}</option>`);
             });
         },
         
     });
 }
+// Laatikoiden borderit näkyivät, joten ne pitää piilottaa, ennenkuin niitä tarvitaan.
 $("#firstly").hide();
 $("#firstly1").hide();
 
+// Tämä funktio tekee sen vaihdoksen ja kertaa myös määrän siihen valuuttaan mitä valitsit.
 function exchange() {
     
 
@@ -48,7 +55,9 @@ function exchange() {
             var final = rate*multiply
             var formattedRate = final.toFixed(2);
             if (multiply > 0 ){
+                // Tässä on se, mitä halusit tietää esim. 5USD Euroissa.
                 $("#firstly").html(`${multiply} USD = ${formattedRate} ${selectedCurrency}<hr><hr>`);
+                // Tässä on se, missä tehdään, eri USD summat olisivat kyseisessä valuutassa.
                 $("#firstly1").html(`1 USD = ${rate} ${selectedCurrency}`);
                 $("#firstly1").append(`<br><hr>5 USD = ${rate*5} ${selectedCurrency}`);
                 $("#firstly1").append(`<br><hr>10 USD = ${rate*10} ${selectedCurrency}`);
@@ -61,7 +70,9 @@ function exchange() {
                 $("#firstly1").append(`<br><hr>10000 USD = ${rate*10000} ${selectedCurrency}`);
 
             }
+            // Jos olet kirjoittanut jotakin muuta tai vain 0 tai alle, tulee kyseinen teksti näkyville.
             else ($("#firstly").html("Write a number above 0!"))
+            
             
         },
         
@@ -70,7 +81,7 @@ function exchange() {
 
 
 
-
+// Hide/Show the rates! nappia painamalla, joko slide up tai down animaatio tapahtuu, jolloin nuo kyseiset ekstra valuuttakurssit lähtevät.
 $("#button2").on("click", function() {
     if ($("#firstly1").is(":visible")) {
         $("#firstly1").slideUp("slow", function() {
@@ -84,7 +95,7 @@ $("#button2").on("click", function() {
 });
 
 
-
+// Tämä funktio tekee sen, että ekstrat avautuvat hitaammin, eikä heti sen haluaman aikana.
 $("#menu").on("change", function() {
     $("#firstly").slideDown("slow", function() {
         setTimeout(function() {
@@ -94,5 +105,5 @@ $("#menu").on("change", function() {
 });
 
 
-
+// Tämä komento tekee sen, että dropdowniin valuutat haetaan heti, kun sivu latautuu.
 $(document).ready(fetchRates);
